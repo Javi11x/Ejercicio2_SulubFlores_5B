9f44ec7b57cfccbdbe62ac7c5fd019213d28df53
Javi11x <84991396+Javi11x@users.noreply.github.com>
2022-01-26 01:07:29 -0600

    Add files via upload

---
+++ Peli.Domain/entities/Pelicula.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Peli.Domain.entities
+{
+    public partial class Pelicula
+    {
+        public int IdPeli { get; set; }
+        public string TituloPeli { get; set; }
+        public string DirectorPeli { get; set; }
+        public string GeneroPeli { get; set; }
+        public int? PuntuacionPeli { get; set; }
+        public int? RatingPeli { get; set; }
+        public string AñopPeli { get; set; }
+    }
+}
